31848d9ac36d204f8fac0ed7ec75e6c66eed5ed1
Ashutosh Singh <ashutosh.singh@sjsu.edu>
2018-12-31 23:53:29 -0800

    Initial Commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './Header.css';
+import logoUrl from '../../assets/logo.svg';
+
+const Header=()=>(
+    <div className="header">
+        <img src={logoUrl} alt="Tesla"></img>
+    </div>
+
+)
+
+export default Header;
+
